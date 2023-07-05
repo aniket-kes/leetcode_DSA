202992c1e80660d21702dc5efb34b65e6fb5a959
Aniket Kesarwani <99314074+aniket-kes@users.noreply.github.com>
2023-07-06 00:12:30 +0530

    Masking

---
+++ leet4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n, mask=0;
+        while(m!=0){
+            mask = (mask<<1) | 1;
+            m = m >> 1;
+        }
+        if(n==0){
+            return 1;
+        }
+
+        return (~n&mask);
+    }
+};
